c90e5a9d5ef12b245075697cab62cf264445fb16
amin <muhammadalamin908@gmail.com>
2025-02-07 07:31:45 +0700

    fix button

--- frontend/src/pages/Home/History.jsx
+++ frontend/src/pages/Home/History.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
-import Button from "../../components/Button/Button";
+import Button from "../../components/button/Button";
 
 const History = ( setUser ) => {
   const [historyData, setHistoryData] = useState([]);
